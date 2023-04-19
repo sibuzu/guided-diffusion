de8050b393037754d70f1bf7252144213fdea0d5
MingShing Su <mingshing.su@gmail.com>
2023-04-19 09:23:18 +0800

    train bat

--- stock_train.bat
+++ stock_train.bat
@@ -1,4 +1,4 @@
-set DATADIR=".\data\TWStock"
+set DATADIR=".\data\TWStock\train"
 
-python stock_train.py --data_dir %DATADIR% --stock_size 64 --num_channels 128 --num_res_blocks 3 --diffusion_steps 4000 --noise_schedule linear --lr 1e-4 --batch_size 1024 
+python stock_train.py --data_dir %DATADIR% --stock_size 64 --num_channels 128 --num_res_blocks 3 --diffusion_steps 4000 --noise_schedule linear --lr 1e-4 --batch_size 1024 --log_dir ./tw_log
 
